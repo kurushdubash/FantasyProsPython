55a27e0d59f78f6076d7fd8ef2b43685fd02e5d7
Kurush Dubash <dubash.kurush@berkeley.edu>
2015-10-10 20:12:33 -0700

    Added menu items for different postions

--- server.py
+++ server.py
@@ -17,12 +17,12 @@ last_updated = update_check()
 def index():
     return render_template('index.html', updated=last_updated)
 
-@app.route('/rankings', methods=['POST'])
+@app.route('/rankings', methods=['GET', 'POST'])
 def rankings():
-    postition = request.form['value']
+    postition = request.form["action"]
     print(postition)
-    #player = get_all_players(postition)
-    return render_template('rankings.html', updated=last_updated,)
+    # player = get_all_players(postition)
+    return render_template('rankings.html', updated=last_updated, type=postition)
 
 
 if __name__ == "__main__":
